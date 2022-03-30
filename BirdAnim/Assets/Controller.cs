7dd2335e22a54c6b769ff57cb4e465e246b233e4
Breanne77 <sjodin@ualberta.ca>
2022-03-30 16:47:05 -0600

    updated rig and imported to unity project, downloaded animation rigging package to unity project

---
+++ BirdAnim/Assets/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float _speed = 1f;
+    private Rigidbody _rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(_rigidBody.velocity.magnitude < _speed){
+            float value = Input.GetAxis("Vertical");
+            if(value != 0){
+                _rigidBody.AddForce(0,0,value*Time.fixedDeltaTime*1000f);
+            }
+        }
+    }
+}
