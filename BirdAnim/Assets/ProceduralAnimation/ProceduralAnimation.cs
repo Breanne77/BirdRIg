de875b188b13ed8ea7d851e2bfe208cc95ebda7b
Breanne77 <sjodin@ualberta.ca>
2022-04-07 23:25:42 -0600

    testing basic target movement

--- BirdAnim/Assets/ProceduralAnimation/ProceduralAnimation.cs
+++ BirdAnim/Assets/ProceduralAnimation/ProceduralAnimation.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class ProceduralAnimation : MonoBehaviour
 {
+    public Transform leftH;
+    public Transform leftR;
+    public Transform leftM;
+    private Vector3 subtract;
     // Start is called before the first frame update
     void Start()
     {
-        
+        subtract = leftH.position + leftR.position;
+        leftH.position = leftH.position-subtract; 
     }
 
     // Update is called once per frame
