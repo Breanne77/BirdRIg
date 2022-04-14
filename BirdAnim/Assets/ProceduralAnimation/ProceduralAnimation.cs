27151cfd0ecc5413711b174b173354f17481381a
Breanne77 <sjodin@ualberta.ca>
2022-04-13 20:22:29 -0600

    added the transforms to the script

--- BirdAnim/Assets/ProceduralAnimation/ProceduralAnimation.cs
+++ BirdAnim/Assets/ProceduralAnimation/ProceduralAnimation.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class ProceduralAnimation : MonoBehaviour
 {
-    public Transform leftH;
-    public Transform leftR;
-    public Transform leftM;
+    public Transform leftHumurus;
+    public Transform leftRadius;
+    public Transform leftMadus;
+    public Transform rightHumurus;
+    public Transform rightRadius;
+    public Transform rightMadus;
+    public Transform root;
+    public Transform tail;
     private Vector3 subtract;
     // Start is called before the first frame update
     void Start()
     {
-        subtract = leftH.position + leftR.position;
-        leftH.position = leftH.position-subtract; 
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        subtract = leftHumurus.position + leftRadius.position;
+        leftHumurus.position = leftHumurus.position-subtract; 
         
+        leftHumurus.position = leftHumurus.position + subtract;
     }
 }
